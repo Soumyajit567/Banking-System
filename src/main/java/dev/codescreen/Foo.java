dcf8db88b87425894c747a8dcb69b71a7b4f5fed
sbx_user1051 <sbx_user1051@169.254.120.141>
2024-04-25 21:27:39 +0000

    Initial commit

---
+++ src/main/java/dev/codescreen/Foo.java
@@ -0,0 +1,17 @@
+package dev.codescreen;
+
+/**
+ * Dummy class with a dummy method.
+ * Rename this class and method to a name that is more appropriate to your coding test.
+ */
+public class Foo {
+
+  /**
+   *
+   *
+   * @return
+   */
+  public static int bar() {
+    return 1;
+  }
+}
